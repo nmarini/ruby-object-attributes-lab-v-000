f53caf9b354ee517d5114a47943cf8e20a957cc6
Nicolas Marini <nmarini44@gmail.com>
2018-04-28 19:25:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-
+  def name=(person_name)
+    @name = person_name
+  end
+  def name 
+    @name
+  end
+  def job=(person_job)
+    @job = person_job
+  end
+  def jon
+    @job
+  end
 end
